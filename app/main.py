c5d17f85ae37c3d69017e5ae70add0867e3bdc46
Eduardo Trujillo <129311642+Lalo64GG@users.noreply.github.com>
2025-07-27 19:30:47 -0600

    Add main entry point for running the FastAPI application

--- app/main.py
+++ app/main.py
@@ -4,3 +4,7 @@ from app.api.data_router import router as data_router
 app = FastAPI()
 
 app.include_router(data_router, prefix="/data", tags=["Data"])
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
